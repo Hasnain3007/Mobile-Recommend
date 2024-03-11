84b46c0894a369752a3fbf63e6e20a8135c26b3a
Muhammad Hasnain Khatri Attari <hasnainkhatri45@gmail.com>
2024-03-11 04:35:00 -0700

    ok

---
+++ app.js
@@ -0,0 +1,227 @@
+var budget = prompt('what is your budget', 'low/high')
+var device = prompt('what oprating system you want', 'Andriod/IOS')
+var camera = prompt('Camera Quality', 'low/medium/high')
+var battery = prompt('Battery Quality', 'short/medium/long')
+var screen = prompt('Screen size', 'small/medium/large')
+
+
+
+if (budget == 'low' && device == 'Android' && camera == 'low' && battery == 'short' && screen == 'small') {
+    alert('you have to buy samsung galaxy j7 prime')
+} else if (budget == 'low' && device == 'Android' && camera == 'low' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy samsung galaxy note 8')
+} else if (budget == 'low' && device == 'Android' && camera == 'low' && battery == 'short' && screen == 'large') {
+    alert('you have to buy techno spark 4')
+} else if (budget == 'low' && device == 'Android' && camera == 'low' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy redmi note 3s')
+} else if (budget == 'low' && device == 'Android' && camera == 'low' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy redmi note 12 pro')
+} else if (budget == 'low' && device == 'Android' && camera == 'low' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy spark 15')
+} else if (budget == 'low' && device == 'Android' && camera == 'low' && battery == 'long' && screen == 'small') {
+    alert('you have to buy infnix hot 10')
+} else if (budget == 'low' && device == 'Android' && camera == 'low' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy infnix hot 11')
+} else if (budget == 'low' && device == 'Android' && camera == 'low' && battery == 'long' && screen == 'large') {
+    alert('you have to buy one plus')
+} else if (budget == 'low' && device == 'Android' && camera == 'medium' && battery == 'short' && screen == 'small') {
+    alert('you have to buy one plus 19')
+} else if (budget == 'low' && device == 'Android' && camera == 'medium' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy samsung galaxy note 8')
+} else if (budget == 'low' && device == 'Android' && camera == 'medium' && battery == 'short' && screen == 'large') {
+    alert('you have to buy techno spark 4')
+} else if (budget == 'low' && device == 'Android' && camera == 'medium' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy redmi note 3s')
+} else if (budget == 'low' && device == 'Android' && camera == 'medium' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy redmi note 12 pro')
+} else if (budget == 'low' && device == 'Android' && camera == 'medium' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy spark 15')
+} else if (budget == 'low' && device == 'Android' && camera == 'medium' && battery == 'long' && screen == 'small') {
+    alert('you have to buy infnix hot 10')
+} else if (budget == 'low' && device == 'Android' && camera == 'medium' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy infnix hot 11')
+} else if (budget == 'low' && device == 'Android' && camera == 'medium' && battery == 'long' && screen == 'large') {
+    alert('you have to buy one plus')
+} else if (budget == 'low' && device == 'Android' && camera == 'high' && battery == 'short' && screen == 'small') {
+    alert('you have to buy one plus 19')
+} else if (budget == 'low' && device == 'Android' && camera == 'high' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy samsung galaxy note 8')
+} else if (budget == 'low' && device == 'Android' && camera == 'high' && battery == 'short' && screen == 'large') {
+    alert('you have to buy techno spark 4')
+} else if (budget == 'low' && device == 'Android' && camera == 'high' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy redmi note 3s')
+} else if (budget == 'low' && device == 'Android' && camera == 'high' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy redmi note 12 pro')
+} else if (budget == 'low' && device == 'Android' && camera == 'high' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy spark 15')
+} else if (budget == 'low' && device == 'Android' && camera == 'high' && battery == 'long' && screen == 'small') {
+    alert('you have to buy infnix hot 10')
+} else if (budget == 'low' && device == 'Android' && camera == 'high' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy infnix hot 11')
+} else if (budget == 'low' && device == 'Android' && camera == 'high' && battery == 'long' && screen == 'large') {
+    alert('you have to buy one plus')
+} else if (budget == 'low' && device == 'IOS' && camera == 'low' && battery == 'short' && screen == 'small') {
+    alert('you have to buy Iphone se')
+} else if (budget == 'low' && device == 'IOS' && camera == 'low' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy Iphone 3')
+} else if (budget == 'low' && device == 'IOS' && camera == 'low' && battery == 'short' && screen == 'large') {
+    alert('you have to buy Iphone 4')
+} else if (budget == 'low' && device == 'IOS' && camera == 'low' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy Iphone 4s')
+} else if (budget == 'low' && device == 'IOS' && camera == 'low' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy Iphone 5')
+} else if (budget == 'low' && device == 'IOS' && camera == 'low' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy Iphone 5s')
+} else if (budget == 'low' && device == 'IOS' && camera == 'low' && battery == 'long' && screen == 'small') {
+    alert('you have to buy Iphone 6')
+} else if (budget == 'low' && device == 'IOS' && camera == 'low' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy Iphone 6s')
+} else if (budget == 'low' && device == 'IOS' && camera == 'low' && battery == 'long' && screen == 'large') {
+    alert('you have to buy Iphone 6s Pro')
+} else if (budget == 'low' && device == 'IOS' && camera == 'medium' && battery == 'short' && screen == 'small') {
+    alert('you have to buy Iphone 7')
+} else if (budget == 'low' && device == 'IOS' && camera == 'medium' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy Iphone 7 Plus')
+} else if (budget == 'low' && device == 'IOS' && camera == 'medium' && battery == 'short' && screen == 'large') {
+    alert('you have to buy Iphone Iphone 8')
+} else if (budget == 'low' && device == 'IOS' && camera == 'medium' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy Iphone 8 Plus')
+} else if (budget == 'low' && device == 'IOS' && camera == 'medium' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy Iphone X')
+} else if (budget == 'low' && device == 'IOS' && camera == 'medium' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy Iphone Xs')
+} else if (budget == 'low' && device == 'IOS' && camera == 'medium' && battery == 'long' && screen == 'small') {
+    alert('you have to buy Iphone Xs Max')
+} else if (budget == 'low' && device == 'IOS' && camera == 'medium' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy Iphone 11')
+} else if (budget == 'low' && device == 'IOS' && camera == 'medium' && battery == 'long' && screen == 'large') {
+    alert('you have to Iphone SE 2020')
+} else if (budget == 'low' && device == 'IOS' && camera == 'high' && battery == 'short' && screen == 'small') {
+    alert('you have to buy Iphone 11 pro')
+} else if (budget == 'low' && device == 'IOS' && camera == 'high' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy Iphone 11 pro max')
+} else if (budget == 'low' && device == 'IOS' && camera == 'high' && battery == 'short' && screen == 'large') {
+    alert('you have to buy i Pod')
+} else if (budget == 'low' && device == 'IOS' && camera == 'high' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy Iphone 3')
+} else if (budget == 'low' && device == 'IOS' && camera == 'high' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy Iphone w')
+} else if (budget == 'low' && device == 'IOS' && camera == 'high' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy Iphone Y')
+} else if (budget == 'low' && device == 'IOS' && camera == 'high' && battery == 'long' && screen == 'small') {
+    alert('you have to buy Iphone 1')
+} else if (budget == 'low' && device == 'IOS' && camera == 'high' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy Iphone v')
+} else if (budget == 'low' && device == 'IOS' && camera == 'high' && battery == 'long' && screen == 'large') {
+    alert('you have to buy Iphone P')
+} else if (budget == 'high' && device == 'Android' && camera == 'low' && battery == 'short' && screen == 'small') {
+    alert('you have to buy samsung galaxy j7 prime')
+} else if (budget == 'high' && device == 'Android' && camera == 'low' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy samsung galaxy note 8')
+} else if (budget == 'high' && device == 'Android' && camera == 'low' && battery == 'short' && screen == 'large') {
+    alert('you have to buy techno spark 4')
+} else if (budget == 'high' && device == 'Android' && camera == 'low' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy redmi note 3s')
+} else if (budget == 'high' && device == 'Android' && camera == 'low' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy redmi note 12 pro')
+} else if (budget == 'high' && device == 'Android' && camera == 'low' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy spark 15')
+} else if (budget == 'high' && device == 'Android' && camera == 'low' && battery == 'long' && screen == 'small') {
+    alert('you have to buy infnix hot 10')
+} else if (budget == 'high' && device == 'Android' && camera == 'low' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy infnix hot 11')
+} else if (budget == 'high' && device == 'Android' && camera == 'low' && battery == 'long' && screen == 'large') {
+    alert('you have to buy one plus')
+} else if (budget == 'high' && device == 'Android' && camera == 'medium' && battery == 'short' && screen == 'small') {
+    alert('you have to buy one plus 19')
+} else if (budget == 'high' && device == 'Android' && camera == 'medium' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy samsung galaxy note 8')
+} else if (budget == 'high' && device == 'Android' && camera == 'medium' && battery == 'short' && screen == 'large') {
+    alert('you have to buy techno spark 4')
+} else if (budget == 'high' && device == 'Android' && camera == 'medium' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy redmi note 3s')
+} else if (budget == 'high' && device == 'Android' && camera == 'medium' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy redmi note 12 pro')
+} else if (budget == 'high' && device == 'Android' && camera == 'medium' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy spark 15')
+} else if (budget == 'high' && device == 'Android' && camera == 'medium' && battery == 'long' && screen == 'small') {
+    alert('you have to buy infnix hot 10')
+} else if (budget == 'high' && device == 'Android' && camera == 'medium' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy infnix hot 11')
+} else if (budget == 'high' && device == 'Android' && camera == 'medium' && battery == 'long' && screen == 'large') {
+    alert('you have to buy one plus')
+} else if (budget == 'high' && device == 'Android' && camera == 'high' && battery == 'short' && screen == 'small') {
+    alert('you have to buy one plus 19')
+} else if (budget == 'high' && device == 'Android' && camera == 'high' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy samsung galaxy note 8')
+} else if (budget == 'high' && device == 'Android' && camera == 'high' && battery == 'short' && screen == 'large') {
+    alert('you have to buy techno spark 4')
+} else if (budget == 'high' && device == 'Android' && camera == 'high' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy redmi note 3s')
+} else if (budget == 'high' && device == 'Android' && camera == 'high' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy redmi note 12 pro')
+} else if (budget == 'high' && device == 'Android' && camera == 'high' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy SAMSUNG Galaxy S24')
+} else if (budget == 'high' && device == 'Android' && camera == 'high' && battery == 'long' && screen == 'small') {
+    alert('you have to buy SAMSUNG Galaxy S24 ULTRA')
+} else if (budget == 'high' && device == 'Android' && camera == 'high' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy SAMSUNG Galaxy S25')
+} else if (budget == 'high' && device == 'Android' && camera == 'high' && battery == 'long' && screen == 'large') {
+    alert('you have to buy SAMSUNG Galaxy S25 ULTRA')
+} else if (budget == 'high' && device == 'IOS' && camera == 'low' && battery == 'short' && screen == 'small') {
+    alert('you have to buy Iphone 7s')
+} else if (budget == 'high' && device == 'IOS' && camera == 'low' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy Iphone 7 plus')
+} else if (budget == 'high' && device == 'IOS' && camera == 'low' && battery == 'short' && screen == 'large') {
+    alert('you have to buy Iphone 8')
+} else if (budget == 'high' && device == 'IOS' && camera == 'low' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy Iphone 8 plus')
+} else if (budget == 'high' && device == 'IOS' && camera == 'low' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy Iphone X')
+} else if (budget == 'high' && device == 'IOS' && camera == 'low' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy Iphone Xs')
+} else if (budget == 'high' && device == 'IOS' && camera == 'low' && battery == 'long' && screen == 'small') {
+    alert('you have to buy Iphone Xs max')
+} else if (budget == 'high' && device == 'IOS' && camera == 'low' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy Iphone 11')
+} else if (budget == 'high' && device == 'IOS' && camera == 'low' && battery == 'long' && screen == 'large') {
+    alert('you have to buy Iphone 11 pro')
+} else if (budget == 'high' && device == 'IOS' && camera == 'medium' && battery == 'short' && screen == 'small') {
+    alert('you have to buy Iphone 11 pro max')
+} else if (budget == 'high' && device == 'IOS' && camera == 'medium' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy Iphone 12 mini')
+} else if (budget == 'high' && device == 'IOS' && camera == 'medium' && battery == 'short' && screen == 'large') {
+    alert('you have to buy Iphone Iphone 12')
+} else if (budget == 'high' && device == 'IOS' && camera == 'medium' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy Iphone 12 pro')
+} else if (budget == 'high' && device == 'IOS' && camera == 'medium' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy Iphone 12 pro max')
+} else if (budget == 'high' && device == 'IOS' && camera == 'medium' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy Iphone 13 mini')
+} else if (budget == 'high' && device == 'IOS' && camera == 'medium' && battery == 'long' && screen == 'small') {
+    alert('you have to buy Iphone 13')
+} else if (budget == 'high' && device == 'IOS' && camera == 'medium' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy Iphone 13 pro')
+} else if (budget == 'high' && device == 'IOS' && camera == 'medium' && battery == 'long' && screen == 'large') {
+    alert('you have to Iphone SE 2020')
+} else if (budget == 'high' && device == 'IOS' && camera == 'high' && battery == 'short' && screen == 'small') {
+    alert('you have to buy Iphone 13 pro Max')
+} else if (budget == 'high' && device == 'IOS' && camera == 'high' && battery == 'short' && screen == 'medium') {
+    alert('you have to buy Iphone 14 mini')
+} else if (budget == 'high' && device == 'IOS' && camera == 'high' && battery == 'short' && screen == 'large') {
+    alert('you have to buy Iphone 14')
+} else if (budget == 'high' && device == 'IOS' && camera == 'high' && battery == 'medium' && screen == 'small') {
+    alert('you have to buy Iphone 14 pro')
+} else if (budget == 'high' && device == 'IOS' && camera == 'high' && battery == 'medium' && screen == 'medium') {
+    alert('you have to buy Iphone 14 pro max')
+} else if (budget == 'high' && device == 'IOS' && camera == 'high' && battery == 'medium' && screen == 'large') {
+    alert('you have to buy Iphone 15 mini')
+} else if (budget == 'high' && device == 'IOS' && camera == 'high' && battery == 'long' && screen == 'small') {
+    alert('you have to buy Iphone 15')
+} else if (budget == 'high' && device == 'IOS' && camera == 'high' && battery == 'long' && screen == 'medium') {
+    alert('you have to buy Iphone 15 pro')
+} else if (budget == 'high' && device == 'IOS' && camera == 'high' && battery == 'long' && screen == 'large') {
+    alert('you have to buy Iphone 15 pro max')
+} else {
+    alert('!!! Wrong Spell')
+}
